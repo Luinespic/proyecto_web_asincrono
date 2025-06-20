9ad82c543da24cc710881066486b93676b25a02e
Luinespic <lucia_49_92@hotmail.com>
2025-06-20 17:16:58 +0200

    deleted footer. Fix import link

--- components/footer/footer.js
+++
@@ -1,9 +0,0 @@
-import "./footer.css";
-
-const template = () => `<p>Made with love by Lucía</p>`;
-
-const Footer = () => {
-  document.querySelector("footer").innerHTML = template();
-};
-
-export default Footer;
--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 import "./style.css";
-import Header from "./components/header/header";
-import Main from "./components/main/main";
-import Footer from "./components/footer/footer";
+import Header from "./components/Header/header";
+import Main from "./Components/Main/main";
 
 const pageBuild = () => {
   Header();
@@ -56,23 +55,3 @@ const addSearchListener = () => {
 };
 
 pageBuild();
-
-/*
-param	            Description
-query	            Search terms.
-page	            Page number to retrieve. (Optional; default: 1)
-per_page	        Number of items per page. (Optional; default: 10)
-order_by	        How to sort the photos. (Optional; default: relevant). Valid values are latest and relevant.
-collections	      Collection ID(‘s) to narrow search. Optional. If multiple, comma-separated.
-content_filter	  Limit results by content safety. (Optional; default: low). Valid values are low and high.
-color	            Filter results by color. Optional. Valid values are: black_and_white, black, white, yellow, orange, red, purple, magenta, green, teal, and blue.
-orientation	      Filter by photo orientation. Optional. (Valid values: landscape, portrait, squarish) 
-
-fetch(
-  "https://api.unsplash.com/search/photos?client_id=DzAareXPVr-vl_h08LmNTeLe1Gb1bpt7o93YDFqieL4&page=1&query=dog"
-)
-  .then((res) => {
-    const img = document.querySelector("#prueba");
-    console.log(res);
-  })
-  .catch((err) => console.log(err));*/
